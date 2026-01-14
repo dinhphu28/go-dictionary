60e9efe835d68642ea60706e143a2e20aeb02f7c
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-01-14 10:24:45 +0700

    Update build script
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,36 @@
 
 set -e
 
-BIN=cmd/dictionary
-
 build() {
-  GOOS=$1 GOARCH=$2 OUT=$3
-  echo "→ Building $OUT"
+  GOOS=$1 GOARCH=$2 OUT_DIR=$3 OUT_BIN=$4
+  echo "→ Building $OUT_BIN ($OUT_DIR)"
   GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 \
-    go build -o dist/$OUT/$4 ./$BIN
+    go build -o dist/$OUT_DIR/$OUT_BIN ./$BIN_SRC
 }
 
-build linux amd64 linux-amd64 dictionary
-build linux arm64 linux-arm64 dictionary
-build darwin amd64 darwin-amd64 dictionary
-build darwin arm64 darwin-arm64 dictionary
-build windows amd64 windows-amd64 dictionary.exe
+build_all() {
+  BIN=$1
+  build linux amd64 linux-amd64 $BIN
+  build linux arm64 linux-arm64 $BIN
+  build darwin amd64 darwin-amd64 $BIN
+  build darwin arm64 darwin-arm64 $BIN
+  build windows amd64 windows-amd64 $BIN.exe
+}
+
+copy_config() {
+  OUT_DIR=$1
+  echo "→ Copying config.json"
+  cp ./configs/config.json dist/$OUT_DIR/config.json
+}
+
+BIN_SRC=cmd/dictionary
+build_all dictionary
+
+BIN_SRC=cmd/setup
+build_all setup
+
+copy_config linux-amd64
+copy_config linux-arm64
+copy_config darwin-amd64
+copy_config darwin-arm64
+copy_config windows-amd64
